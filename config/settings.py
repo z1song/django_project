1a4c1f822180e1fbf0c12517086e86dc93e5b42c
z1-song <wcn07102@naver.com>
2020-12-07 02:01:29 +0900

    add debug false

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bfsnb+arscyz%g3hoy%rh$*%q@=e_mp=w^w61awk1ia+xvvf(m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
